de07f3f873bf17d6fae5d797882bc3007043b1e4
Yunishello <yunishello@gmail.com>
2021-11-18 17:41:05 +0100

    Farm4me Landing Page

---
+++ js/drop.js
@@ -0,0 +1,38 @@
+$(function(){
+        
+        var list    = $("#myList"),
+          listItem  = $("> ul li",list),
+          listCount = listItem.length;
+          
+        function expand(){
+          listItem.each(function(i){
+            $(this).attr("style", "z-index:"+i+";"
+              + "-webkit-animation-delay:" + i * 100 + "ms;"
+                 + "-moz-animation-delay:" + i * 100 + "ms;"
+                   + "-o-animation-delay:" + i * 100 + "ms;"
+                      + "animation-delay:" + i * 100 + "ms;");
+            });
+          list.removeClass("collapse").addClass("expand");
+        }
+
+        function collapse(){
+          listItem.each(function(i){
+            i++;
+            $(this).attr("style", "z-index:"+i+";"
+              + "-webkit-animation-delay:" + (listCount-i) * 100 + "ms;"
+                 + "-moz-animation-delay:" + (listCount-i) * 100 + "ms;"
+                   + "-o-animation-delay:" + (listCount-i) * 100 + "ms;"
+                      + "animation-delay:" + (listCount-i) * 100 + "ms;");
+            });
+          list.removeClass("expand").addClass("collapse");
+        }
+
+        $("span").bind("click", function(){
+          if (list.hasClass("expand")) {
+            collapse();
+          }else{
+            expand();
+          }
+        });
+        
+      });
